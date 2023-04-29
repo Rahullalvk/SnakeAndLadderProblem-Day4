44982bc14b063f98d5fdfecc804a9cec8933225b
Rahulalvk <rahulal432@gmail.com>
2023-04-28 22:02:40 -0700

    The Player then checks for a Option. They are No Play,Ladder or Snake

--- SnakeAndLader/SnakeAndLader/SnakeAndLadder.cs
+++ SnakeAndLader/SnakeAndLader/SnakeAndLadder.cs
@@ -3,20 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
     {
-        public static void DisplaySnakeAndLadder() 
+        public static void DisplaySnakeAndLadder()
         {
             int playerPosition = 0;
             Random random = new Random();
-            int roll = random.Next(1, 7);
-            Console.WriteLine($"You rolled a {roll}.");
+            while (playerPosition < 100)
+            {
+                int roll = random.Next(1, 7);
+                Console.WriteLine($"You rolled a {roll}.");
 
-            playerPosition += roll;
+                int option = random.Next(0, 3); // 0 = No Play, 1 = Ladder, 2 = Snake
 
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play. You stay in the same position.");
+                        break;
+                    case 1:
+                        int ladderSteps = random.Next(1, 7);
+                        Console.WriteLine($"You landed on a ladder! Move up {ladderSteps} positions.");
+                        playerPosition += ladderSteps;
+                        break;
+                    case 2:
+                        int snakeSteps = random.Next(1, 7);
+                        Console.WriteLine($"You landed on a snake! Move back {snakeSteps} positions.");
+                        playerPosition -= snakeSteps;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+            }
         }
+
+
+
+
+
+
+        
     }
 }
