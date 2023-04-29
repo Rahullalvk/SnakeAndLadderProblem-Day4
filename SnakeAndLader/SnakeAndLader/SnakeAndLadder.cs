e2597896ed983f7c70dbf4673251146ac0000b68
Rahulalvk <rahulal432@gmail.com>
2023-04-28 21:46:25 -0700

    Player start at position 0 and plyer rolls the die to get a nuber between 1 to 6

---
+++ SnakeAndLader/SnakeAndLader/SnakeAndLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        public static void DisplaySnakeAndLadder() 
+        {
+            int playerPosition = 0;
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine($"You rolled a {roll}.");
+
+            playerPosition += roll;
+
+        }
+    }
+}
